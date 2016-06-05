a5962b7f66226e69950aeee75faadabe7a742d1f
Oleksii Tkachuk <kelebra20@gmail.com>
2016-06-05 15:10:48 -0500

    Changed representation layer. Added connectivity to mongo

--- backend/src/main/resources/pages/js/blog.js
+++ backend/src/main/resources/pages/js/blog.js
@@ -11,9 +11,9 @@ function BlogControl(headingElement, postsElement) {
         header('All posts');
     };
 
-    this.post = function(id) {
+    this.post = function(title) {
         clear();
-        $.get("/post/" + id, function(data) {
+        $.get("/post/" + title.replace(' ', '%20'), function(data) {
             postsElement.append(render(data));
             header(data.title);
             Prism.highlightAll();
@@ -25,14 +25,31 @@ function BlogControl(headingElement, postsElement) {
     };
 
     function render(json) {
+        var section = $("<section class='post'><header class='post-header'><header class='post-header'><h2 class='post-title' id='title'></h2><p class='post-meta'><div id='tags'></div></p></header><div class='post-description' id='content'></div></section>");
+
         var html = $(json.content);
         var title = json.title;
-        var heading = html.find('h2').first();
-        heading.attr('style', 'cursor: pointer');
-        heading.click(function() {
+        var tags = json.tags;
+        var added = json.added;
+
+        section.find('#content').append(html);
+
+        var tagsElement = section.find('#tags');
+        for(var i in tags) {
+            tagsElement.append($("<a class='post-category post-category-design' href='#'>" + tags[i] + "</a>"));
+        }
+
+        var titleElement = section.find('#title');
+        titleElement.html(title);
+        titleElement.attr('style', 'cursor: pointer');
+        titleElement.click(function() {
             blogControl.post(title);
         });
-        return html;
+
+        var createdAt = new Date(added);
+        section.find('#added').html('' + createdAt.getFullYear() + '-' + (createdAt.getMonth() + 1) + '-' + createdAt.getDate());
+
+        return section;
     };
 
     function header(text) {
