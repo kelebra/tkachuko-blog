cbdbe98b387420850a3229149db3681e9f2e1074
Oleksii Tkachuk <kelebra20@gmail.com>
2017-10-09 18:32:13 -0500

    Added tests for query creation

--- src/test/scala/com/tkachuko/blog/repository/source/http/RequestCompositionTest.scala
+++ src/test/scala/com/tkachuko/blog/repository/source/http/RequestCompositionTest.scala
@@ -1,16 +1,26 @@
 package com.tkachuko.blog.repository.source.http
 
-import com.tkachuko.blog.repository.source.http.EntityAccessActor.Query
+import com.tkachuko.blog.repository.source.http.PostAccessActor.ReadPost
+import com.tkachuko.blog.repository.source.http.PostInfoActor.ReadPostInfos
 import org.scalatest.{Matchers, WordSpec}
 
 class RequestCompositionTest extends WordSpec with Matchers {
 
   "Request composition" should {
 
-    "create http request for present filter parameter" in {
-      new Query {
-        override val filter = None
-      }
+    "create http request for read post by title" in {
+      val query = ReadPost("This is some post title")
+      val collection = "some-collection"
+
+      val httpRequest = EntityAccessActor.request(collection, query)
+      httpRequest.path should endWith(s"""$collection?q={"title":"${query.title}"}""")
+    }
+
+    "create http request for read of all post infos" in {
+      val collection = "some-collection"
+
+      val httpRequest = EntityAccessActor.request(collection, ReadPostInfos)
+      httpRequest.path should endWith(s"""$collection?f={"content":0}""")
     }
   }
 }
