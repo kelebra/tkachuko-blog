e1d8009c2bde427c40b7697f3c75c164d42ee93e
kelebra <kelebra20@gmail.com>
2018-12-09 19:54:27 -0800

    Coming back to disqus, sigh

--- src/main/scala/com/tkachuko/blog/frontend/views/CommentsView.scala
+++ src/main/scala/com/tkachuko/blog/frontend/views/CommentsView.scala
@@ -1,19 +1,38 @@
 package com.tkachuko.blog.frontend.views
 
-import org.scalajs.dom
 import org.scalajs.dom.Element
 import org.scalajs.dom.html.{Div, Script}
 import scalatags.JsDom.all._
 
-class CommentsView {
+class CommentsView(title: String) {
 
   def render(container: Element) = {
     container.appendChild(commentsContainer)
-    container.appendChild(setup)
+    container.appendChild(loader)
   }
 
-  private def commentsContainer: Div =
-    div(id := "commento").render
+  private def commentsContainer: Div = div(id := "disqus_thread", `class` := "item").render
+
+  private def loader = script(
+    raw(
+      s"""
+         |    var disqus_config = function () {
+         |        this.page.url = '';
+         |        this.page.identifier = '${title.hashCode}';
+         |        this.page.title = '$title';
+         |    };
+         |
+         |    (function() {
+         |        var d = document, s = d.createElement('script');
+         |
+         |        s.src = '//tkachuko-info.disqus.com/embed.js';
+         |
+         |        s.setAttribute('data-timestamp', +new Date());
+         |        (d.head || d.body).appendChild(s);
+         |    })();
+    """.stripMargin
+    )
+  ).render
 
   private def setup: Script =
     script(src := "https://cdn.commento.io/js/commento.js").render
@@ -21,8 +40,6 @@ class CommentsView {
 
 object CommentsView {
 
-  lazy val noscript = typedTag[dom.html.Head]("noscript")
-
-  def apply(): CommentsView = new CommentsView()
+  def apply(title: String) = new CommentsView(title)
 }
 
